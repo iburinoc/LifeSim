c6589213cbd399d18a414d868077fa09e602db71
Andrey Boris Khesin <andrey.khesin@gmail.com>
2013-12-09 18:28:38 -0500

    Resyntaxed everything in threedee.

--- src/life/threedee/Matrix.java
+++ src/life/threedee/Matrix.java
@@ -1,37 +1,36 @@
 package life.threedee;
 
-public class Matrix{
+public class Matrix {
 	private double[][] matrix;
 	
-	public Matrix(double[][] matrix){
+	public Matrix (double[][] matrix) {
 		this.matrix = matrix;
 	}
 	
-	public double _(int x,int y){
-		try{
+	public double _ (int x, int y) {
+		try {
 			return matrix[y][x];
-		}
-		catch(ArrayIndexOutOfBoundsException e){
+		} catch(ArrayIndexOutOfBoundsException e) {
 			e.printStackTrace();
 			return Double.NaN;
 		}
 	}
 	
-	public Matrix submatrixMissingRowAndColumn(int row, int column) {
-	    double[][] newMatrix = new double[matrix.length-1][matrix[0].length-1];
+	public Matrix submatrixMissingRowAndColumn (int row, int column) {
+	    double[][] newMatrix = new double[matrix.length - 1][matrix[0].length - 1];
 	    for (int i = 0; i < matrix.length; i++) {
 	        for (int j = 0; j < matrix[0].length; j++) {
 	            if (i > row) {
 	                if (j > column) {
-	                    newMatrix[i-1][j-1]=_(i,j);
+	                    newMatrix[i - 1][j - 1] = _(i, j);
 	                } else if (j < column) {
-	                    newMatrix[i-1][j]=_(i,j);
+	                    newMatrix[i - 1][j] = _(i, j);
 	                }
 	            } else if (i < row) {
 	                if (j > column) {
-	                    newMatrix[i][j-1]=_(i,j);
+	                    newMatrix[i][j - 1] = _(i, j);
 	                } else if (j < column) {
-	                    newMatrix[i][j]=_(i,j);
+	                    newMatrix[i][j] = _(i, j);
 	                }
 	            }
 	        }
@@ -39,16 +38,16 @@ public class Matrix{
 	    return new Matrix(newMatrix);
 	}
 	
-	public double determinant(){
+	public double determinant () {
 		if (matrix.length != matrix[0].length) {
 		    throw new IllegalArgumentException("Not a square matrix");
 		}
 		if (matrix.length == 1) {
-		    return _(0,0);
+		    return _(0, 0);
 		} else {
 		    double determinantSum = 0;
 		    for (int i = 0; i < matrix.length; i++) {
-		        determinantSum += _(i,0)*this.submatrixMissingRowAndColumn(i, 0).determinant()*(i%2==0?1:-1);
+		        determinantSum += _(i, 0) * this.submatrixMissingRowAndColumn(i, 0).determinant() * (i % 2 == 0 ? 1 : -1);
 		    }
 		    return determinantSum;
 		}
--- src/life/threedee/Vector.java
+++ src/life/threedee/Vector.java
@@ -1,54 +1,52 @@
 package life.threedee;
 
-public class Vector{
-	public final double x,y,z;
+public class Vector {
+	public final double x, y, z;
 	public final double s;
 	
-	public Vector(double x,double y,double z){
+	public Vector (double x, double y, double z){
 		double mag = Math.sqrt(x * x + y * y + z * z);
-		
 		this.x = x;
 		this.y = y;
 		this.z = z;
 		this.s = mag;
 	}
 	
-	public Vector(Point p0, Point p1){
-		this.x = (p1.x - p0.x);
-		this.y = (p1.y - p0.y);
-		this.z = (p1.z - p0.z);
+	public Vector (Point point0, Point point1) {
+		this.x = (point1.x - point0.x);
+		this.y = (point1.y - point0.y);
+		this.z = (point1.z - point0.z);
 		double mag = Math.sqrt(x * x + y * y + z * z);
-		
 		this.s = mag;
 	}
 	
-	public Vector unit(){
+	public Vector unitVector () {
 		double mag = Math.sqrt(x * x + y * y + z * z);
 		return new Vector(x / mag, y / mag, z / mag);
 	}
 	
-	public String toString(){
-		return "(" + x + "," + y + "," + z + "," + s + ")";
+	public String toString() {
+		return "(" + x + ", " + y + ", " + z + ", " + s + ")";
 	}
 	
-	public Vector crossProduct(Vector v){
+	public Vector crossProduct (Vector v){
 		Vector u = this;
 		double u1 = u.x;
 		double u2 = u.y;
 		double u3 = u.z;
-		
+
 		double v1 = v.x;
 		double v2 = v.y;
 		double v3 = v.z;
-		
-		Matrix x = new Matrix(new double[][]{{u2,u3},{v2,v3}});
-		Matrix y = new Matrix(new double[][]{{u1,u3},{v1,v3}});
-		Matrix z = new Matrix(new double[][]{{u1,u2},{v1,v2}});
-		
-		return new Vector(x.determinant(),-y.determinant(),z.determinant());
+
+        Matrix x = new Matrix(new double[][]{{u2, u3}, {v2, v3}});
+        Matrix y = new Matrix(new double[][]{{u1, u3}, {v1, v3}});
+        Matrix z = new Matrix(new double[][]{{u1, u2}, {v1, v2}});
+
+        return new Vector(x.determinant(), -y.determinant(), z.determinant());
 	}
 	
-	public double dotProduct(Vector v){
-		return x * v.x + y * v.y + z * v.z;
+	public double dotProduct(Vector vector){
+		return x * vector.x + y * vector.y + z * vector.z;
 	}
 }

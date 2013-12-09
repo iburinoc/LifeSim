0ab79f31c7f66dba1e2ded90aa55b6aacf0e3a39
Iburinoc <iburinoc@gmail.com>
2013-12-09 18:41:42 -0500

    Camera class.  That is all.

---
+++ src/life/threedee/Camera.java
@@ -0,0 +1,14 @@
+package life.threedee;
+
+import java.util.List;
+
+public class Camera{
+	
+	private Point loc;
+	private Vector dir;
+	
+	public Camera(double x,double y,double z, List<Plane> objects){
+		
+	}
+	
+}

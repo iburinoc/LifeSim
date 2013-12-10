a7ed5ea1b0db83ffa8c7182eaedfb0b72921429b
Iburinoc <iburinoc@gmail.com>
2013-12-10 09:05:52 -0500

    More thingamajig with camera

--- src/life/threedee/Camera.java
+++ src/life/threedee/Camera.java
@@ -27,14 +27,28 @@ public class Camera{
 		this(new Point(0,0,0),new Vector(0,0,1));
 	}
 	
-	private Vector getVectorForPixel(int x,int y){
+	private Vector getVectorForPixel(int x,int y, Vector right, Vector up){
 		int rx = screenWidth / 2 - x;
 		int ry = screenHeight / 2 - y;
 		double px = rx * dx;
 		double py = ry * dy;
+		
+		Vector nright = right.setScal(px);
+		Vector nup = right.setScal(py);
+		return nup.add(nright);
 	}
 
-	private Plane closestInFront(){
-
+	private Plane closestInFront(List<Plane> objects, Vector dir, Point px){
+		double minT = Double.POSITIVE_INFINITY;
+		Plane minPlane = null;
+		for(Plane p : objects){
+			double t = p.calculateT(dir, px);
+			if(minT > t){
+				minT = t;
+				minPlane = p;
+			}
+		}
+		
+		return minPlane;
 	}
 }

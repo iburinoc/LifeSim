6210ab326b64ab42cbb0a42845a07baaad515334
iburinoc <iburinoc@gmail.com>
2013-12-09 19:34:15 -0500

    Math for screen size and shtuff worked out.  Begin official making of camera tings (and by that i mean math inbound.  alert! math inbound.)

--- src/life/threedee/Camera.java
+++ src/life/threedee/Camera.java
@@ -7,12 +7,20 @@ public class Camera{
 	private Point loc;
 	private Vector dir;
 	
+	private double 
+			width  = 1.53465397596, 
+			height = 1.15099048197;
+	private int
+			screenWidth  = 480,
+			screenHeight = 360;
+	
 	public Camera(double x,double y,double z){
 		
 	}
 	
 	private Vector getVectorForPixel(int x,int y){
-		
+		int rx = screenWidth / 2 - x;
+		int ry = screenHeight / 2 - y;
 	}
 	
 	private Plane closestInFront(){

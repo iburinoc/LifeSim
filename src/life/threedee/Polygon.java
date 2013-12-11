85e98444b54448762317a6b5ef06ff163f4b31e9
Andrey Boris Khesin <andrey.khesin@gmail.com>
2013-12-11 18:43:05 -0500

    Minor fixes.

--- src/life/threedee/Polygon.java
+++ src/life/threedee/Polygon.java
@@ -1,6 +1,6 @@
 package life.threedee;
 
-import java.awt.*;
+import java.awt.Color;
 
 public class Polygon extends Plane{
     private Point[] vertices;
@@ -13,17 +13,21 @@ public class Polygon extends Plane{
         this.up = up;
     }
 
-    public Polygon(Point[] vertices, Vector right, Point3D origin, Vector normal, Color colour) {
+    public Polygon(Point[] vertices, Vector right, Vector up, Point3D origin, Vector normal, Color colour) {
         super (origin, normal, colour);
         this.vertices = vertices;
         this.right = right;
+        this.up = up;
     }
 
     public boolean inside(Point point) {
         boolean inside = true;
         for (int i = 0; i < vertices.length; i++) {
             try {
-                Line side = new Line((vertices[i].y - vertices[(i + 1) % vertices.length].y) / (vertices[i].x - vertices[(i + 1) % vertices.length].x), vertices[i].y - (vertices[i].y - vertices[(i + 1) % vertices.length].y) / (vertices[i].x - vertices[(i + 1) % vertices.length].x) * vertices[i].x);
+                Line side = new Line((vertices[i].y - vertices[(i + 1) % vertices.length].y) /
+                                     (vertices[i].x - vertices[(i + 1) % vertices.length].x),
+                                      vertices[i].y - (vertices[i].y - vertices[(i + 1) % vertices.length].y) /
+                                     (vertices[i].x - vertices[(i + 1) % vertices.length].x) * vertices[i].x);
                 if (side.over(vertices[(i + 2) % vertices.length]) != side.over(point)) {
                     inside = false;
                 }
@@ -39,10 +43,6 @@ public class Polygon extends Plane{
 
     @Override
     public Point3D intersection(Vector vector, Point3D point3D) {
-        double t = super.calculateT(vector, point3D);
-        double nx = point3D.x + vector.x * t;
-        double ny = point3D.y + vector.y * t;
-        double nz = point3D.z + vector.z * t;
-        return inside(new Point(new Plane (super.intersection(vector, point3D), right).calculateT(right, super.intersection(vector, point3D)), new Plane (super.intersection(vector, point3D), up).calculateT(up, super.intersection(vector, point3D)))) ? new Point3D(nx, ny, nz) : null;
+        return inside(new Point(new Plane (super.intersection(vector, point3D), right).calculateT(right, super.intersection(vector, point3D)), new Plane (super.intersection(vector, point3D), up).calculateT(up, super.intersection(vector, point3D)))) ? super.intersection(vector, point3D) : null;
     }
 }

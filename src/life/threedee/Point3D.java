bfbc11a285254e5ffe4c3e0aeddbb59659d2cbe2
Andrey Boris Khesin <andrey.khesin@gmail.com>
2013-12-12 19:27:48 -0500

    Goodbye to classes. Too bad so sad.

--- src/life/threedee/Point3D.java
+++ src/life/threedee/Point3D.java
@@ -1,12 +1,13 @@
 package life.threedee;
 
-public class Point3D extends Point {
-	public final double z;
+public class Point3D {
+    public final double x, y, z;
 
 	public Point3D(double x, double y, double z){
-		super(x, y);
-		this.z = z;
-	}
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
 
     @Override
 	public String toString(){

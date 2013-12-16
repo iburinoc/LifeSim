91cc1c3e26696c03e6130e8d832bad151b2b6db6
Iburinoc <iburinoc@gmail.com>
2013-12-16 11:00:29 -0500

    Not necessary any more, sorry meat tree

--- src/life/engine/creatures/Carnivore.java
+++
@@ -1,5 +0,0 @@
-package life.engine.creatures;
-
-public interface Carnivore extends Creature{
-	public void feed(Creature c);
-}
--- src/life/engine/creatures/Herbivore.java
+++
@@ -1,7 +0,0 @@
-package life.engine.creatures;
-
-import life.engine.resources.Resource;
-
-public interface Herbivore extends Creature{
-	public void feed(Resource resource);
-}

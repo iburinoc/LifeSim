666b2e6cea05b8cc52fcc75e02b5b68bf8438140
doome1337 <dmitry.paramonov@gmail.com>
2014-01-06 12:37:32 -0500

    HTTPlanes now work! Time to remake goasts.

--- src/life/threedee/HalvedTrapezoidalTexturedPlane.java
+++ src/life/threedee/HalvedTrapezoidalTexturedPlane.java
@@ -13,13 +13,17 @@ public class HalvedTrapezoidalTexturedPlane extends TexturedPlane {
     public Color c(Point inter) {
         Vector p = new Vector(this.origin, inter);
         double du = p.dotProduct(super.up);
-        double dr = p.dotProduct(right);
+        double dr = p.dotProduct(super.right);
         int py = texture.getHeight() - (int) (du * PX_METER);
-        double pv = (2*h-py)/2*h;
-        int px = (int) (dr * PX_METER * pv);
-        if(px >= 0 && px < w && py >= 0 && py < h) {
+        double pv = (((double)super.h)+((double)py))/(2*((double)super.h));
+        int px = (int) (dr * PX_METER / pv);
+        /*System.out.print(":D");
+        System.out.println(py);
+        System.out.println(h);
+        System.out.println(pv);*/
+        if(px >= 0 && px < super.w && py >= 0 && py < super.h) {
             try{
-                return new Color(texture.getRGB(px, py));
+                return new Color(super.texture.getRGB(px, py));
             }
             catch(ArrayIndexOutOfBoundsException e) {
             }

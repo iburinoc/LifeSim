590e0eeb1a81cad87c34e61fc01a34ad92d37176
Andrey Boris Khesin <andrey.khesin@gmail.com>
2014-01-07 00:17:50 -0500

    YAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAY!!!!!!!!!!!!!!!!!!!!

--- src/life/threedee/Plane.java
+++ src/life/threedee/Plane.java
@@ -56,7 +56,8 @@ public class Plane implements ThreeDeeObject{
 	}
 
     public boolean sameSide(Point point1, Point point2){
-        return (Math.abs(calculateT(new Vector(point2.subtract(point1)), point1)) >= 1);
+        double t = calculateT(new Vector(point2.subtract(point1)), point1);
+        return t >= 1 || t <= 0;
     }
 
     @Override

f0b79f9403adcce4f64c29600350405a38d4ef37
iburinoc <iburinoc@gmail.com>
2014-01-08 08:57:55 -0500

    pellets list ya firkin bugger

--- src/life/threedee/game/maps/GameMap.java
+++ src/life/threedee/game/maps/GameMap.java
@@ -1,14 +1,18 @@
 package life.threedee.game.maps;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 import life.threedee.Point;
 import life.threedee.ThreeDeeObject;
+import life.threedee.game.Pellet;
 
 public class GameMap {
 	private List<ThreeDeeObject> objects;
 	private Map<MapLocation, List<ThreeDeeObject>> map;
+	
+	private List<Pellet> pelletsList;
 	
 	public GameMap() {
 		objects = MapBuilder.createMap();
@@ -21,5 +25,17 @@ public class GameMap {
 	
 	public List<ThreeDeeObject> getObjects(Point pos) {
 		return map.get(new MapLocation(pos.x, pos.z));
+	}
+	
+	public List<Pellet> pelletsList() {
+		if(pelletsList == null) {
+			pelletsList = new ArrayList<Pellet>();
+			for(ThreeDeeObject o : objects) {
+				if(o instanceof Pellet) {
+					pelletsList.add((Pellet) o);
+				}
+			}
+		}
+		return pelletsList;
 	}
 }

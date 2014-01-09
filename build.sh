5d7b06a6e584dab89130a8bde0e37be8148fa3de
Iburinoc <iburinoc@gmail.com>
2014-01-09 17:49:43 -0500

    build script

---
+++ build.sh
@@ -0,0 +1,11 @@
+rm -rv bin
+mkdir bin
+cp -rv resources bin/resources
+cd src
+javac -d ../bin life/threedee/game/Game.java
+cd ../bin
+mkdir META-INF
+touch META-INF/MANIFEST.MF
+echo Main-Class: life.threedee.game.Game > META-INF/MANIFEST.MF
+zip -r 3DPacman.jar *
+mv 3DPacman.jar ..

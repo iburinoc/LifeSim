bfade13e5b4f775d9348caa2493bce98ca6b20ec
Iburinoc <iburinoc@gmail.com>
2014-01-16 10:32:42 -0500

    it works :D

--- server/server.js
+++ server/server.js
@@ -14,10 +14,45 @@ function writeScores(response) {
 	response.end();
 }
 
+function addScore(data) {
+	var i = 0;
+	console.log(data.name + ";" + data.score);
+	while(i < 10) {
+		if(score['score'+i] == undefined || data.score > score['score'+i]) {
+			var j = 8;
+			console.log('score added at ' + i);
+			while(j >= i) {
+				if(score['name'+j] !== undefined) {
+					score['name'+(j+1)] = score['name'+j];
+					score['score'+(j+1)] = score['score'+j];
+				}
+				j--;
+			}
+			score['name'+i] = data.name;
+			score['score'+i]= data.score;
+			i = 10;
+		}
+		i++;
+	}
+	console.log(score);
+	fs.writeFile('./score.txt', JSON.stringify(score), function(){});
+}
+
 function onRequest(request, response) {
 	console.log(request.url);
 	if(request.method == 'GET') {
-		writeScores(response); 
+		writeScores(response);
+	}else if(request.method == 'POST') {
+		request.on('data', function(data) {
+			console.log("Post Request: " + data);
+			try{
+				addScore(JSON.parse(data));
+			}
+			catch(err) {
+				console.log("Invalid post request");
+			}
+			writeScores(response);
+		});
 	}
 }
 

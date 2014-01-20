cfb303bd289e9090d83d428f9107cb169d6cd379
doome1337 <dmitry.paramonov@gmail.com>
2014-01-19 22:21:27 -0500

    GHD has been commented.

--- src/life/threedee/game/GhostHouseDoor.java
+++ src/life/threedee/game/GhostHouseDoor.java
@@ -6,10 +6,24 @@ import life.threedee.Point;
 import life.threedee.Vector;
 import life.threedee.game.maps.MapFeature;
 
+/**
+ * The door to the ghost house, which is transparent, but prevents user from entering it. 
+ *
+ * @author Andrey Boris Khesin
+ * @author Dmitry Andreevich Paramonov
+ * @author Sean Christopher Papillon Purcell
+ *
+ */
 public class GhostHouseDoor extends Plane implements MapFeature {
 
+    //It's id, once again, for deterministic generation.
 	private final int id;
 	
+	/**
+	 * Constructs a new GhostHouseDoor with the given origin and normal.
+	 * @param p The origin point.
+	 * @param n The normal vector.
+	 */
 	public GhostHouseDoor(Point p, Vector n) {
 		super(p, n, GameUtilities.BLANK);
 		id = GameUtilities.idCount++;
@@ -19,6 +33,7 @@ public class GhostHouseDoor extends Plane implements MapFeature {
 		return id;
 	}
 
+	// Whether or not a given Point p is contained within the area defined by the door.
 	private boolean contained(Point p) {
 		Vector d = new Vector(this.origin, p);
 		return d.x >= 0 && d.x < 2 && d.y >= 0 && d.y <= WALL_HEIGHT && d.z == 0;

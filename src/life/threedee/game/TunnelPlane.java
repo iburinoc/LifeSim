8c0916be984364ac0f05dd4f4e9bf2c67b722a13
Andrey Boris Khesin <andrey.khesin@gmail.com>
2014-01-19 23:39:27 -0500

    Tunnel Plane commented.

--- src/life/threedee/game/TunnelPlane.java
+++ src/life/threedee/game/TunnelPlane.java
@@ -6,14 +6,20 @@ import life.threedee.Plane;
 import life.threedee.Point;
 import life.threedee.Vector;
 
+/**
+ * This is a special kind of plane that there are two instances of, on either side of the map. If you see a tunnel plane, the game knows to draw what is on the other side.
+ */
 public class TunnelPlane extends Plane{
+    /**
+     * Represent whether the plane is on the right side of the map of the left side.
+     */
 	public final double id;
-	
-    public TunnelPlane(Point a, Point b, Point c){
-        super(a,b,c, a.x > 0 ? Color.MAGENTA : Color.GREEN);
-        id = Math.signum(a.x);
-    }
 
+    /**
+     * "Default" constructor. Works like the regular plane constructor.
+     * @param origin A point on the plane. Doesn't matter where on the plane. It is used to determine what side of the map the plane is on.
+     * @param normal The normal to the plane.
+     */
     public TunnelPlane(Point origin, Vector normal){
         super(origin, normal, Color.BLACK);
         id = Math.signum(origin.x);

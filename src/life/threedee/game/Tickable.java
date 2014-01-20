96df9e692357206a919b65ddccfd3c1ca3734256
doome1337 <dmitry.paramonov@gmail.com>
2014-01-19 19:39:28 -0500

    I javadoc’d Tickable.

--- src/life/threedee/game/Tickable.java
+++ src/life/threedee/game/Tickable.java
@@ -1,5 +1,16 @@
 package life.threedee.game;
 
+/**
+ * An interface representing an object that does an action per tick of the game.
+ * 
+ * @author Andrey Boris Khesin
+ * @author Dmitry Andreevich Paramonov
+ * @author Sean Christopher Papillon Purcell
+ *
+ */
 public interface Tickable{
+    /**
+     * The action that is to be performed each tick of the game.
+     */
 	public void tick();
 }
